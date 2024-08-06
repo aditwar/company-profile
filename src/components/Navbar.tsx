692e5d432234f8fc7b2420327e882cdda615faa5
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-06 23:28:57 +0700

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar() {
   return (
     <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
       <div className="flex items-center">
-        <Image src={Logo} alt="Logo" />
+        <Image src={Logo} alt="Logo" width={50} className="lg:m-5" />
 
         <div className="hidden lg:flex pl-[74px] gap-x-[56px]">
           {navLinks.map((item, index) => (
