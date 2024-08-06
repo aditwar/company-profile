1fde65de9c3e198bf7b8fc2246ff8ec741a88948
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-07 01:26:00 +0700

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,46 @@
+import React from 'react';
 import Image from "next/image";
+import Link from 'next/link';
 import Logo from "../../public/svg/Logo.svg";
-import User from "../../public/svg/User.svg";
-import Menu from "../../public/svg/Menu.svg";
+
 
 const navLinks = [
-  { name: "Features" },
-  { name: "Pricing" },
-  { name: "Enterprise" },
-  { name: "Careers" },
+  { name: "Home" },
+  { name: "About Us" },
+  { name: "Offer" },
+  { name: "Teams" },
 ];
 
 export function Navbar() {
   return (
-    <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
-      <div className="flex items-center">
-        <Image src={Logo} alt="Logo" width={50} className="lg:m-5" />
+    <>
+      <nav className="fixed flex w-full shadow-xl backdrop-blur-md items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20  z-50">
+        <div className="flex items-center ">
+          <Link href={'/'}>
+            <Image src={Logo} alt="Logo" width={50} className="lg:m-5" />
+          </Link>
 
-        <div className="hidden lg:flex pl-[74px] gap-x-[56px]">
-          {navLinks.map((item, index) => (
-            <p className="text-[#36485C] font-medium" key={index}>
-              {item.name}
-            </p>
-          ))}
-        </div>
+          <div className="hidden lg:flex pl-[74px] gap-x-[60px]">
+            {navLinks.map((item, index) => (
+              <p className="text-[#36485C] font-medium" key={index}>
+                {item.name}
+              </p>
+            ))}
+          </div>
+        </div>      
+      </nav>
+      
+      <div>
+        <ul className='hidden lg:flex'>         
+          <li className='ml-10 hover: border-b text.xl'><Link href={"/"}>Home</Link></li>
+          
+          <li className='ml-10 hover: border-b text.xl'><Link href={"/"}>About Us</Link></li>
+
+          <li className='ml-10 hover: border-b text.xl'><Link href={"/"}>Over Team</Link></li>
+
+          <li className='ml-10 hover: border-b text.xl'><Link href={"/"}>Teams</Link></li>
+        </ul>
       </div>
-
-      <div className="flex gap-x-5">
-        <p className="hidden lg:block font-medium text-[#36485C] pr-[56px]">
-          Open an Account
-        </p>
-
-        <div className="flex items-center gap-x-2">
-          <Image src={User} alt="User Profile" />
-          <span className="hidden font-medium text-[#36485C] lg:block">
-            Sign in
-          </span>
-        </div>
-
-        <Image src={Menu} alt="Menu Button" className="lg:hidden" />
-      </div>
-    </nav>
+    </>
   );
 }
