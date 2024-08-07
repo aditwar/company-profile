e1b0196ba30b5c5f5b538c1808c6fbfbc7188b2a
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-07 15:10:16 +0700

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from 'next/link';
 import Logo from "../../public/svg/Logo.svg";
 import hamburger from "../../public/svg/hamburger.svg";
+import { AiOutlineClose } from 'react-icons/ai';
 import { useState } from "react"; // untuk menggunakan HOOK
 
 
@@ -12,7 +13,7 @@ import { useState } from "react"; // untuk menggunakan HOOK
 const navLinks = [
   { name: "Home" },
   { name: "About Us" },
-  { name: "Offer" },
+  { name: "Service" },
   { name: "Teams" },
 ];
 
@@ -39,21 +40,47 @@ export function Navbar() {
               </p>
             ))}
           </div>
-        </div>
-        
-        {/* Hamburger Menu */}
-        <div onClick={handleNav} className="justify-center items-center md:hidden cursor-pointer p-[50px]">
-          <Image src={hamburger} alt="hamburger" width={25} className="" />        
-        </div>
-
-        <div className={
-          menuOpen
-          ? "fixed left-0 top-0 w-[65%] md:hidden h-screen bg-white p-10 ease-in duration-500"
-          : "fixed left-[-100%] top-0 p-0 ease-in duration-500"
-        }>
-
-        </div>
+        </div>               
       </nav>
+
+      {/* Hamburger Menu */}
+      <div onClick={handleNav} className="fixed items-center md:hidden right-[10%] p-[50px] z-10">
+          <Image src={hamburger} alt="hamburger" width={25} className="" />        
+      </div>
+
+      <div className='fixed w-full z-10'>
+        <div className={
+            menuOpen
+            ? "fixed left-0 top-0 w-[40%] backdrop-blur-md md:hidden h-screen bg-gradient-to-r from-cyan-500 p-10 ease-in duration-500"
+            : "fixed left-[-100%] top-0 px-0 ease-in duration-500"
+          }>
+
+          <div className='flex w-full items-center justify-end p-[10px]'>
+            <div onClick={handleNav} className='cursor-pointer'>
+              <AiOutlineClose size={25} />
+            </div>
+          </div>
+
+          <div className='flex-col py-4'>
+            <ul>
+              <li 
+                onClick={() => setMenuOpen(false)}
+                className='py-4 font-extrabold size-fit cursor-pointer'><Link href="/"/>Home</li>
+              <li
+                onClick={() => setMenuOpen(false)}
+                className='py-4 font-extrabold size-fit cursor-pointer'><Link href="/about-us"/>About Us</li>
+              <li
+              onClick={() => setMenuOpen(false)}
+              className='py-4 font-extrabold size-fit cursor-pointer'><Link href="/service"/>Service</li>
+              <li
+              onClick={() => setMenuOpen(false)}
+              className='py-4 font-extrabold size-fit cursor-pointer'><Link href="/teams"/>Teams</li>
+            </ul>            
+          </div>
+
+          
+        </div>
+      </div>
     </>
   );
 }
