b6dd696e61a469bbf892142d16a4b01d0b684880
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-07 07:59:07 +0700

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
 import Logo from "../../public/svg/Logo.svg";
-import {AiOutlineMenu} from 'react-icons/ai'
+import hamburger from "../../public/svg/hamburger.svg";
 import { useState } from "react"; // untuk menggunakan HOOK
 
 
@@ -24,47 +26,34 @@ export function Navbar() {
   return (
     <>
       <nav className="fixed flex w-full shadow-xl backdrop-blur-md items-center px-[50px] py-[16px] z-10">
-        <div className="flex items-center justify-center ">
+        <div className="flex items-center">
           <Link href={'/'}>
             <Image src={Logo} alt="Logo" width={50} className="md:m-5" />
           </Link>
 
           {/* Navbar Page Default */}
-          <div className="hidden md:flex pl-[74px] gap-x-[100px]">
+          <div className="hidden md:flex pl-[300px] justify-items-center gap-x-[100px]">
             {navLinks.map((item, index) => (
               <p className="text-[#36485C] font-extrabold size-fit" key={index}>
                 {item.name}
               </p>
             ))}
           </div>
-        </div>      
+        </div>
+        
+        {/* Hamburger Menu */}
+        <div onClick={handleNav} className="justify-left md:hidden cursor-pointer pl-24">
+          <Image src={hamburger} alt="hamburger" width={25} className="" />        
+        </div>
+
+        <div className={
+          menuOpen
+          ? "fixed left-0 top-0 w-[65%] md:hidden h-screen bg-white p-10 ease-in duration-500"
+          : "fixed left-[-100%] top-0 p-0 ease-in duration-500"
+        }>
+
+        </div>
       </nav>
-      
-      {/* Navbar saat md */}
-      <div>
-        <ul className="hidden md:flex">         
-          <li className="ml-10 uppercase hover:border-b text-xl"><Link href={"/"}>Home</Link></li>
-          
-          <li className="ml-10 uppercase hover:border-b text-xl"><Link href={"/about-us"}>About Us</Link></li>
-
-          <li className="ml-10 uppercase hover:border-b text-xl"><Link href={"/offer"}>Offer</Link></li>
-
-          <li className="ml-10 uppercase hover:border-b text-xl"><Link href={"/teams"}>Teams</Link></li>
-        </ul>
-      </div>
-
-      {/* Hamburger Menu */}
-      <div onClick={handleNav} className="md:hidden cursor-pointer pl-24">
-        <AiOutlineMenu size={25}/>
-      </div>
-
-      <div className={
-        menuOpen
-        ? "fixed left-0 top-0 w-[65%] md:hidden h-screen bg-white p-10 ease-in duration-500"
-        : "fixed left-[-100%] top-0 p-0 ease-in duration-500"
-      }>
-
-      </div>
     </>
   );
 }
