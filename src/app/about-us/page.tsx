8a98ef1ab0f43edea09168bf76b6f19e2985109f
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-08 11:24:11 +0700

    a

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,8 +9,13 @@ export const metadata: Metadata = {
 
 export default function AboutUsPage() {
     return (
-        <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In aspernatur labore sed amet nobis ab, quam voluptatibus ad consequuntur a explicabo sit, minus inventore. Excepturi officiis non nisi impedit harum quas veniam expedita? Excepturi voluptates error voluptate officiis? Reprehenderit vero et mollitia cum ea, quae quaerat animi magnam sit, pariatur modi dolores sapiente minus aut delectus debitis blanditiis! Reiciendis, veritatis! Nemo facere quia dolorum dignissimos hic sequi itaque temporibus doloremque ipsam provident perferendis, consequuntur libero at aperiam culpa! Laboriosam iste consequatur officia repudiandae at, facilis ea corporis dolores quam neque consequuntur eos modi eligendi expedita quia eius molestias adipisci ipsam provident quod maiores pariatur blanditiis? Soluta id perspiciatis, vel quaerat modi fugit quasi tenetur, laborum ab illo magni. Ipsum a molestiae nostrum voluptates assumenda quam explicabo cum doloremque fuga aliquid voluptatum consectetur quo dolorum magnam obcaecati nobis dolor commodi culpa dolore veritatis, blanditiis aliquam reprehenderit saepe adipisci! Nam numquam est dolor explicabo quis. Commodi magni corporis quam aperiam, autem repellat sequi dicta maxime quae quibusdam deleniti natus animi ad, dolore consequuntur. Error eaque, illo delectus velit deleniti iusto assumenda animi? Minus aut nobis impedit deleniti enim, accusamus repellat culpa corporis, molestiae at quibusdam illum! Molestiae exercitationem odit quisquam commodi dignissimos tempora quasi mollitia nisi sint enim, ipsam temporibus aperiam assumenda error asperiores officiis. Repellendus tempore dignissimos quidem, enim adipisci quae, sed sunt placeat eveniet, est dolores laborum voluptate aperiam at quos. Et ratione distinctio assumenda, recusandae consectetur facilis impedit ea deleniti reiciendis totam qui voluptatem, quibusdam obcaecati quis veniam consequuntur eos autem dolore. Fugiat velit in porro, quae consequatur, repellat itaque aut, expedita voluptate fuga magni dicta ducimus voluptates pariatur perspiciatis accusantium? Labore cumque, harum provident aperiam consequatur libero officia consequuntur beatae! Eius, tempora neque! Itaque voluptate impedit ea sed, reiciendis ipsam perferendis assumenda esse tempore est, consequuntur facere similique?
+        <div className="tracking-[2px] text-white font-bold w-[400px] lg:w-[700px] text-2xl lg:text-4xl text-center mb-28">
+            <h1 className="tracking-[2px] text-white font-bold w-[400px] lg:w-[700px] text-2xl lg:text-4xl text-center mb-28">
+                HEADER
+            </h1>
+            <div className="flex flex-wrap  bg-slate-900 items-center gap-6">
+                <Image src={Google} alt="" />
+            </div>
         </div>
     )
 }
