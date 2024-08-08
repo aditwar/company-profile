279463987fe1180b55c67c499336a6f62348ade0
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-08 15:37:34 +0700

    a

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,7 @@
+
+import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { Metadata } from "next";
 
@@ -9,13 +12,30 @@ export const metadata: Metadata = {
 
 export default function AboutUsPage() {
     return (
-        <div className="tracking-[2px] text-white font-bold w-[400px] lg:w-[700px] text-2xl lg:text-4xl text-center mb-28">
-            <h1 className="tracking-[2px] text-white font-bold w-[400px] lg:w-[700px] text-2xl lg:text-4xl text-center mb-28">
-                HEADER
-            </h1>
-            <div className="flex flex-wrap  bg-slate-900 items-center gap-6">
-                <Image src={Google} alt="" />
+        <>
+            <div className='max-w-7xl mx-auto pt-[180px]'>
+                <ul className='absolute lg:-z-0'>
+                    <li>                        
+                        <div className='absolute mb-10 md:text-end'>
+                            <div className='text-black'>1996</div>
+                            <div className='text-lg text-black font-bold'>Company Establishment</div>
+                            <p className='lg:w-[500px] w-[350px] text-black'>The company was founded by a group of robotics engineers and AI experts with a vision to revolutionize the robotics industry. Initial team formation, including hardware engineers, software, and AI experts. Intensive research and development (R&D) for the first robotic prototype. Launch of the first prototype, Household Assistant Robot.</p>
+                        </div>                        
+                    </li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
-        </div>
+            <div className="tracking-[2px] text-white font-bold w-[400px] lg:w-[700px] text-2xl lg:text-4xl text-center mb-28">
+                <h1 className="tracking-[2px] text-white font-bold w-[400px] lg:w-[700px] text-2xl lg:text-4xl text-center mb-28">
+                    HEADER
+                </h1>
+                <div className="flex flex-wrap  bg-slate-900 items-center gap-6">
+                    
+                </div>
+            </div>
+            
+                    
+        </>
     )
 }
