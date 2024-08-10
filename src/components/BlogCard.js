1d6cfe875e5327508d7bd9280971be984395a46c
Aditya Wardana <wardanaaditya49@gmail.com>
2024-08-10 21:07:03 +0700

    a

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,6 +2,7 @@
 export default function BlogCard({ blog }) {
     const { title, slug, Categori, Date, Content, Image, Author } = blog.fields
 
+
     return (
         <div className="card">
             <div className="blogscss">
@@ -9,6 +10,7 @@ export default function BlogCard({ blog }) {
             </div>
             <div className="content">
                 <div className="info">
+                    <h4>{ title }</h4>
                     
                 </div>
             </div>
